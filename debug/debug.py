b83701a5540121a40b810b6681c06ef3cfa0be59
Keren Cohen <keren.c91@gmail.com>
2023-10-30 15:50:38 +0200

    Updating debug tests for loss comparison to include more comparison options
    Updating requirements.txt file to include the new packages

--- debug/debug.py
+++ debug/debug.py
@@ -2,8 +2,11 @@ import numpy as np
 import torch
 import aspire
 import matplotlib.pyplot as plt
-import yaml
 import os
+import yaml
+from collections import OrderedDict
+import matplotlib.patches as mpatches
+import random
 
 Namespace = dict
 
@@ -43,6 +46,9 @@ def get_loss(f_loss, f_loss_ent, f_loss_prior, f_loss_data, num_epoch):
     return (data_loss[num_epoch], data_loss_ent[num_epoch],
             data_loss_prior[num_epoch], data_loss_data[num_epoch])
 
+def generate_random_color():
+    return tuple(random.random() for _ in range(4))
+
 def plot_losses_comparison(*args, **kwargs):
     '''
     args - arguments containing list of .npy loss file paths
@@ -52,18 +58,32 @@ def plot_losses_comparison(*args, **kwargs):
     '''
 
     # Get the number of total args and kwargs
-    num_args = len(args)
+    args_list = args[0]
+    num_args = len(args_list)
     num_kwargs = len(kwargs)
 
     losses_list = []
     i = 0
-    jump_size = 100
+    jump_size = kwargs.get('jump_size', None)
     # Create lambda function taking values in jumps of jump_size
     jump_over_array = lambda array: array[::jump_size]
+    base_folder = kwargs.get('base_folder', None)
+    loss_file = kwargs.get('loss_file', None)
+    clip_epochs = kwargs.get('clip_epochs', None)
+    # Get minimal number of epochs
+    min_epoch = 99001
+    if clip_epochs:
+        for arg in args_list:
+            # Load loss data per session
+            path = os.path.join(base_folder, arg, loss_file)
+            data_loss = np.load(path)
+            min_epoch = min(len(data_loss), 100000) # 100000 is the default value
     # Loop over sessions
-    for arg in args:
+    for arg in args_list:
         # Load loss data per session
-        data_loss = np.load(arg)
+        path = os.path.join(base_folder, arg, loss_file)
+        data_loss = np.load(path)
+        data_loss = data_loss[:min_epoch-1]
         # Get jumps array
         data_loss_jumps = jump_over_array(data_loss)
         # Append to list of losses to plot
@@ -71,22 +91,69 @@ def plot_losses_comparison(*args, **kwargs):
         i += 1
 
     output_folder = kwargs.get('output_folder', None)
+    output_file = kwargs.get('output_file', None)
+
     title = kwargs.get('diff', "No diff found")
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
     plt.figure()
+    comparison_details = kwargs.get('comparison_details', None)
     for i in range(num_args):
-        plt.plot(losses_list[i], label=i)
+        plt.plot(losses_list[i], label=comparison_details[i])
     plt.legend()
-    plt.yscale('log')
-    plt.title(f'{title}')
-    plt.savefig(f'./{output_folder}/losses_comp.png')
+    plt.title(f'{title}, epochs={min_epoch}')
+    plt.savefig(f'./{output_folder}/{output_file}.png')
     plt.close()
 
-    # Write the dictionary to a YAML file
-    with open(f'./{output_folder}/params_comp.yaml', "w") as f:
-        yaml.dump(args, f)
 
+def get_folders_list(comparison_param, comparison_details):
+    folders = []
+
+    if comparison_param == "num_imgs":
+        latent_type = "gmm"
+        sigma = 0.5
+        latent_dim = 40
+        folders = [
+            f"MNIST648_{latent_type}_denoising_deepdecoder_{comp_param}imgs_{sigma}noise_std_dropout0.0001_layer_size150x6_latent{latent_dim}_seed100_eps0.001"
+            for comp_param in comparison_details
+        ]
+    elif comparison_param == "latent_dim":
+        latent_type = "gmm"
+        num_imgs = 5
+        sigma = 0.5
+        folders = [
+            f"MNIST648_{latent_type}_denoising_deepdecoder_{num_imgs}imgs_{sigma}noise_std_dropout0.0001_layer_size150x6_latent{comp_param}_seed100_eps0.001"
+            for comp_param in comparison_details
+        ]
+    elif comparison_param == "sigma":
+        latent_type = "gmm"
+        num_imgs = 5
+        latent_dim = 40
+        folders = [
+            f"MNIST648_{latent_type}_denoising_deepdecoder_{num_imgs}imgs_{comp_param}noise_std_dropout0.0001_layer_size150x6_latent{latent_dim}_seed100_eps0.001"
+            for comp_param in comparison_details
+        ]
+    elif comparison_param == "latent_type":
+        num_imgs = 5
+        sigma = 1.0
+        latent_dim = 40
+        folders = [
+            f"MNIST648_{comp_param}_denoising_deepdecoder_{num_imgs}imgs_{sigma}noise_std_dropout0.0001_layer_size150x6_latent{latent_dim}_seed100_eps0.001"
+            for comp_param in comparison_details
+        ]
+    else:
+        raise ValueError(f"Undefined comparison param: {comparison_param}")
+    return folders
+
+
+def write_comparison_args(output_folder, output_file, comparison_param, comparison_details, folders):
+    labels = OrderedDict([
+        ("comparison_param", comparison_param),
+        ("comparison_details", comparison_details),
+        ("folders", folders),
+    ])
+    with open(f'./{output_folder}/params_comp_{comparison_param}.yaml', "w") as f:
+        yaml.dump(labels, f)
 
 # def test_model(model_pt, data_mrc):
 #
--- debug/debug_tests.py
+++ debug/debug_tests.py
@@ -1,10 +1,33 @@
 import debug
-
+import yaml
 
 # Plot losses comparison test
-debug.plot_losses_comparison("../results_batched/MNIST648_gmm_denoising_deepdecoder_5imgs_0.5noise_std_dropout0.0001_layer_size150x6_latent8_seed100_eps0.001/loss.npy",
-                             "../results_batched/MNIST648_gmm_denoising_deepdecoder_5imgs_0.5noise_std_dropout0.0001_layer_size150x6_latent40_seed100_eps0.001/loss.npy",
-                             "../results_batched/MNIST648_gmm_denoising_deepdecoder_35imgs_0.5noise_std_dropout0.0001_layer_size150x6_latent40_seed100_eps0.001/loss.npy",
-                             "../results_batched/MNIST648_gmm_denoising_deepdecoder_50imgs_0.5noise_std_dropout0.0001_layer_size150x6_latent8_seed100_eps0.001/loss.npy",
-                             output_folder="../results_batched/comparison")
-                             #diff="num_imgs:5, 5, 35, 50. latent_dim:8, 40, 40, 8")
+loss_names = ["loss", "loss_data", "loss_ent", "loss_prior"]
+comparison_params = ["num_imgs", "latent_dim", "sigma", "latent_type"]
+comparison_details_dict = {
+    "num_imgs": ['5', '35', '75', '150'],
+    "latent_dim": ['8', '40', '150'],
+    "sigma": ['0.5', '1.0', '2.0'],
+    "latent_type": ['gmm', 'gmm_custom', 'gmm_eye']
+}
+# Compare
+for comparison_param in comparison_params:
+    comparison_details = comparison_details_dict[comparison_param]
+    for loss in loss_names:
+        output_folder = "../results_batched/comparison"
+        output_file = f"{loss}_comparison_{comparison_param}"
+        folders = debug.get_folders_list(comparison_param, comparison_details)
+        comparison_details_string = ",".join(comparison_details)
+        debug.plot_losses_comparison(folders,
+                                     base_folder="../results_batched",
+                                     loss_file=f"{loss}.npy",
+                                     output_folder=output_folder,
+                                     output_file=output_file,
+                                     diff=f"Compare {comparison_param}: {comparison_details_string}. {loss}",
+                                     jump_size=100,
+                                     clip_epochs=False,
+                                     comparison_details=comparison_details)
+
+    # Write the comparison details to a YAML file
+    debug.write_comparison_args(output_folder, output_file, comparison_param, comparison_details, folders)
+
